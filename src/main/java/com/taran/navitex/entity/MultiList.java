912b4029a08bba114a03dcd486956bcf270e3fee
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-16 11:49:08 +0300

    Add custom classes which will be used in searching algorithm and help to find searching path

---
+++ src/main/java/com/taran/navitex/entity/MultiList.java
@@ -0,0 +1,39 @@
+package com.taran.navitex.entity;
+
+public class MultiList {
+    private int[] head;
+    private int[] next;
+    private int[] vert;
+    private int[] cost;
+    private int cnt = 1;
+
+    public MultiList(int pointsNumber, int edgeNumber) {
+        head = new int[pointsNumber];
+        next = new int[edgeNumber + 1];
+        vert = new int[edgeNumber + 1];
+        cost = new int[edgeNumber + 1];
+    }
+
+    public int[] getHead() {
+        return head;
+    }
+
+    public int[] getNext() {
+        return next;
+    }
+
+    public int[] getVert() {
+        return vert;
+    }
+
+    public int[] getCost() {
+        return cost;
+    }
+
+    public void add(int u, int v, int w) {
+        next[cnt] = head[u];
+        vert[cnt] = v;
+        cost[cnt] = w;
+        head[u] = cnt++;
+    }
+}
---
+++ src/main/java/com/taran/navitex/entity/RMQ.java
@@ -0,0 +1,39 @@
+package com.taran.navitex.entity;
+
+import java.util.Arrays;
+
+public class RMQ {
+    private int INF = Integer.MAX_VALUE / 2;
+    private int n;
+    private int[] val;
+    private int[] ind;
+
+    public RMQ(int size) {
+        n = size;
+        val = new int[2 * n];
+        ind = new int[2 * n];
+        Arrays.fill(val, INF);
+        for (int i = 0; i < n; i++)
+            ind[n + i] = i;
+    }
+
+    public void set(int index, int value) {
+        val[n + index] = value;
+        for (int v = (n + index) / 2; v > 0; v /= 2) {
+            int l = 2 * v;
+            int r = l + 1;
+            if (val[l] <= val[r]) {
+                val[v] = val[l];
+                ind[v] = ind[l];
+            } else {
+                val[v] = val[r];
+                ind[v] = ind[r];
+            }
+        }
+    }
+
+    public int minIndex() {
+        return val[1] < INF ? ind[1] : -1;
+    }
+
+}
