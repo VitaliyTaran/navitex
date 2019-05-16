4d2885563bfbc669792979250d4c4d85228e6270
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-16 13:03:18 +0300

    Created custom exception to algorithm

---
+++ src/main/java/com/taran/navitex/exception/LogicAlgorithmException.java
@@ -0,0 +1,8 @@
+package com.taran.navitex.exception;
+
+public class LogicAlgorithmException extends Exception {
+
+    public LogicAlgorithmException(String message) {
+        super(message);
+    }
+}
