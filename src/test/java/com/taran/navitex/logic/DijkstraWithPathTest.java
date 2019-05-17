27b6077aec62aec2efc349f4bb7bc1c0d103211d
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-17 12:15:56 +0300

    remake test algorithm

--- src/test/java/com/taran/navitex/logic/DijkstraWithPathTest.java
+++ src/test/java/com/taran/navitex/logic/DijkstraWithPathTest.java
@@ -2,7 +2,7 @@ package com.taran.navitex.logic;
 
 import com.taran.navitex.entity.Edge;
 import com.taran.navitex.entity.Graph;
-import com.taran.navitex.entity.Point;
+import com.taran.navitex.entity.Sensor;
 import com.taran.navitex.exception.LogicAlgorithmException;
 import org.junit.Test;
 
@@ -14,23 +14,31 @@ public class DijkstraWithPathTest {
     @Test
     public void dijkstraRMQ() throws LogicAlgorithmException {
         List<Edge> edgeList = new ArrayList<>();
-        List<Point> pointList = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            pointList.add(new Point(i, String.valueOf(i)));
+        List<Sensor> pointList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            pointList.add(new Sensor(i, String.valueOf(i)));
         }
         edgeList.add(new Edge(pointList.get(0), pointList.get(1), 1));
-        edgeList.add(new Edge(pointList.get(0), pointList.get(3), 5));
-        edgeList.add(new Edge(pointList.get(1), pointList.get(2), 2));
-        edgeList.add(new Edge(pointList.get(2), pointList.get(3), 1));
-        edgeList.add(new Edge(pointList.get(3), pointList.get(4), 1));
-        edgeList.add(new Edge(pointList.get(4), pointList.get(5), 2));
-        edgeList.add(new Edge(pointList.get(4), pointList.get(6), 4));
-        edgeList.add(new Edge(pointList.get(5), pointList.get(6), 1));
+        edgeList.add(new Edge(pointList.get(1), pointList.get(0), 1));
+
+        edgeList.add(new Edge(pointList.get(1), pointList.get(2), 1));
+        edgeList.add(new Edge(pointList.get(2), pointList.get(1), 1));
+
+        edgeList.add(new Edge(pointList.get(2), pointList.get(3), 2));
+        edgeList.add(new Edge(pointList.get(3), pointList.get(2), 2));
+
+        edgeList.add(new Edge(pointList.get(0), pointList.get(3), 1));
+        edgeList.add(new Edge(pointList.get(3), pointList.get(0), 1));
+
+        edgeList.add(new Edge(pointList.get(0), pointList.get(4), 1));
+        edgeList.add(new Edge(pointList.get(4), pointList.get(0), 1));
+
+
 
 
         Graph graph = new Graph(pointList, edgeList);
         AlgorithmDijkstra dijkstra = new AlgorithmDijkstra();
-        dijkstra.execute(graph, 0, 6);
+        dijkstra.execute(graph, 1, 4);
         System.out.println(dijkstra.getCost());
         System.out.println(dijkstra.getRecoveredPath());
     }
