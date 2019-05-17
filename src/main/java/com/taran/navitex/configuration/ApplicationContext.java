462be9be70a99b22f1351f17c2b7cbbcb7c3b980
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-17 12:10:15 +0300

    Made Spring application context

---
+++ src/main/java/com/taran/navitex/configuration/ApplicationContext.java
@@ -0,0 +1,59 @@
+package com.taran.navitex.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:postgre_db.properties")
+@ComponentScan(basePackages = {})
+public class ApplicationContext {
+    private Environment environment;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        config.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
+        config.setUsername(environment.getRequiredProperty("jdbc.username"));
+        config.setPassword(environment.getRequiredProperty("jdbc.password"));
+        config.setMaximumPoolSize(20);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManager() {
+        Properties properties = new Properties();
+        properties.put("javax.persistence.jdbc.driver", environment.getRequiredProperty("jdbc.driverClassName"));
+        properties.put("javax.persistence.jdbc.url", environment.getRequiredProperty("jdbc.url"));
+        properties.put("javax.persistence.jdbc.user", environment.getRequiredProperty("jdbc.username"));
+        properties.put("javax.persistence.jdbc.password", environment.getRequiredProperty("jdbc.password"));
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.epam.travelagency.entity");
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(properties);
+        return em;
+    }
+
+
+}
