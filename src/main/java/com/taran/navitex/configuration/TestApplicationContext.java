bd333b5465d686692f43a43ef96d692c3f8df42f
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-20 09:58:02 +0300

    Cheated test context

---
+++ src/main/java/com/taran/navitex/configuration/TestApplicationContext.java
@@ -0,0 +1,26 @@
+package com.taran.navitex.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestApplicationContext extends ApplicationContext {
+    public TestApplicationContext(Environment environment) {
+        super(environment);
+    }
+
+    @Override
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
+        embeddedDatabaseBuilder.setType(EmbeddedDatabaseType.H2);
+        embeddedDatabaseBuilder.generateUniqueName(true);
+        embeddedDatabaseBuilder.setScriptEncoding("UTF-8");
+        embeddedDatabaseBuilder.addScript("classpath:db/migration/V1.1___creation_db.sql");
+        embeddedDatabaseBuilder.addScript("classpath:db/migration/V1.2__init_data.sql");
+        return embeddedDatabaseBuilder.build();
+    }
+}
