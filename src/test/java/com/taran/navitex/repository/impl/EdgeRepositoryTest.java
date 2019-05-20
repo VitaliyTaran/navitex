1522d092483e429add946f5e469f558454eb48e1
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-20 11:53:53 +0300

    Made repositories tests

---
+++ src/test/java/com/taran/navitex/repository/impl/EdgeRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.taran.navitex.repository.impl;
+
+import com.taran.navitex.configuration.TestApplicationContext;
+import com.taran.navitex.data.TestCollections;
+import com.taran.navitex.entity.Edge;
+import com.taran.navitex.entity.Sensor;
+import com.taran.navitex.repository.EdgeRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestApplicationContext.class})
+public class EdgeRepositoryTest {
+    @Autowired
+    private EdgeRepository repository;
+    private List<Edge> edgeList;
+    private List<Sensor> sensorList;
+    private Edge testEdge;
+
+    @Before
+    public void setUp() {
+        TestCollections testCollections = new TestCollections();
+        edgeList = testCollections.getEdgeList();
+        sensorList = testCollections.getSensorList();
+        testEdge = new Edge(sensorList.get(5), sensorList.get(4), 1);
+    }
+
+    @Test
+    public void find() {
+        Optional<Edge> expected = repository.find(1, 2);
+        Optional<Edge> actual = edgeList.stream().filter(o -> o.getFirst().getId() == 1 && o.getSecond().getId() == 2).findFirst();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() {
+        List<Edge> expected = repository.findAll();
+        Assert.assertEquals(expected, edgeList);
+    }
+
+    @Test
+    public void create() {
+        repository.create(testEdge);
+        testEdge.setId(11);
+        testEdge.setFirst(sensorList.get(5));
+        testEdge.setSecond(sensorList.get(4));
+        edgeList.add(testEdge);
+
+        List<Edge> expected = repository.findAll();
+        Assert.assertEquals(expected, edgeList);
+    }
+
+    @Test
+    public void update() {
+        testEdge.setId(1);
+        repository.update(testEdge);
+        edgeList.set(0, testEdge);
+        List<Edge> expected = repository.findAll();
+        Assert.assertEquals(expected, edgeList);
+    }
+
+    @Test
+    public void remove() {
+        repository.remove(1, 2);
+        Optional<Edge> first = edgeList.stream()
+                .filter(o -> o.getFirst().getId() == 1 && o.getSecond().getId() == 2)
+                .findFirst();
+        edgeList.remove(first.get());
+        Assert.assertEquals(repository.findAll(), edgeList);
+    }
+
+
+}
---
+++ src/test/java/com/taran/navitex/repository/impl/SensorRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.taran.navitex.repository.impl;
+
+import com.taran.navitex.configuration.TestApplicationContext;
+import com.taran.navitex.data.TestCollections;
+import com.taran.navitex.entity.Sensor;
+import com.taran.navitex.repository.SensorRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestApplicationContext.class})
+public class SensorRepositoryTest {
+    @Autowired
+    private SensorRepository repository;
+    private List<Sensor> sensorList;
+    private Sensor testSensor;
+
+
+    @Before
+    public void setUp() {
+        TestCollections testCollections = new TestCollections();
+        sensorList = testCollections.getSensorList();
+        testSensor = new Sensor("7", 75.7575, 54.5454);
+    }
+
+
+    @Test
+    public void find() {
+        Sensor expected = repository.find(1).get();
+        Sensor actual = sensorList.get(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() {
+        List<Sensor> expected = repository.findAll();
+        Assert.assertEquals(expected, sensorList);
+    }
+
+    @Test
+    public void create() {
+
+        repository.create(testSensor);
+        testSensor.setId(7);
+        sensorList.add(testSensor);
+
+        List<Sensor> expected = repository.findAll();
+        Assert.assertEquals(expected, sensorList);
+    }
+
+    @Test
+    public void update() {
+        testSensor.setId(5);
+        testSensor.setName("5");
+        sensorList.set(4, testSensor);
+
+        repository.update(testSensor);
+
+        List<Sensor> expected = repository.findAll();
+        Assert.assertEquals(expected, sensorList);
+    }
+
+    @Test
+    public void remove() {
+        repository.remove(6);
+        sensorList.remove(5);
+
+        List<Sensor> expected = repository.findAll();
+        Assert.assertEquals(expected, sensorList);
+    }
+
+
+}
