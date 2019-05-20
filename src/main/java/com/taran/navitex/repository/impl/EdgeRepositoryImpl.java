38b9ecc215afc38416d36e003f2b4ea7f0574eae
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-20 11:51:43 +0300

    Reworked create query

--- src/main/java/com/taran/navitex/repository/impl/EdgeRepositoryImpl.java
+++ src/main/java/com/taran/navitex/repository/impl/EdgeRepositoryImpl.java
@@ -15,6 +15,7 @@ public class EdgeRepositoryImpl implements EdgeRepository {
     private static final String DELETE_EDGE_SQL = "DELETE FROM edge where first_sensor_id=? and second_sensor_id=?";
     private static final String FIND_ALL_EDGE_SQL = "SELECT * FROM edge";
     private static final String FIND_EDGE_SQL = "SELECT * FROM edge where first_sensor_id=? and second_sensor_id=?";
+    private static final String CREATE_EDGE_SQL = "INSERT INTO edge( first_sensor_id, second_sensor_id, cost) VALUES ( ?, ?, ?);";
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -22,7 +23,7 @@ public class EdgeRepositoryImpl implements EdgeRepository {
     @Override
     public Optional<Edge> find(long first_sensor_id, long second_sensor_id) {
         entityManager.joinTransaction();
-        Query query = entityManager.createNativeQuery(FIND_EDGE_SQL);
+        Query query = entityManager.createNativeQuery(FIND_EDGE_SQL, Edge.class);
         query.setParameter(1, first_sensor_id);
         query.setParameter(2, second_sensor_id);
         return Optional.of((Edge) query.getSingleResult());
@@ -37,14 +38,17 @@ public class EdgeRepositoryImpl implements EdgeRepository {
 
     @Override
     public void create(Edge edge) {
-        entityManager.persist(edge);
-
+        entityManager.joinTransaction();
+        Query query = entityManager.createNativeQuery(CREATE_EDGE_SQL);
+        query.setParameter(1, edge.getFirst().getId());
+        query.setParameter(2, edge.getSecond().getId());
+        query.setParameter(3, edge.getCost());
+        query.executeUpdate();
     }
 
     @Override
     public void update(Edge edge) {
         entityManager.merge(edge);
-
     }
 
     @Override
@@ -55,7 +59,7 @@ public class EdgeRepositoryImpl implements EdgeRepository {
     @Override
     public boolean remove(long first_sensor_id, long second_sensor_id) {
         entityManager.joinTransaction();
-        Query query = entityManager.createNativeQuery(DELETE_EDGE_SQL);
+        Query query = entityManager.createNativeQuery(DELETE_EDGE_SQL, Edge.class);
         query.setParameter(1, first_sensor_id);
         query.setParameter(2, second_sensor_id);
         return query.executeUpdate() == 1;
