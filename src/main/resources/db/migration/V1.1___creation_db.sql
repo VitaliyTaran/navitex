464556e0a890e57d1fbab9a0d5afcc98ff850302
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-21 00:46:00 +0300

    reworked scripts

--- src/main/resources/db/migration/V1.1___creation_db.sql
+++ src/main/resources/db/migration/V1.1___creation_db.sql
@@ -3,7 +3,8 @@ CREATE TABLE sensor
     id        SERIAL primary key,
     name      varchar(250),
     latitude  double precision,
-    longitude double precision
+    longitude double precision,
+    mark      int
 );
 
 create table edge
@@ -12,6 +13,8 @@ create table edge
     first_sensor_id  int,
     second_sensor_id int,
     cost             int,
+    distance         double precision,
+    average_mark     float,
     foreign key (first_sensor_id) references sensor (id),
     foreign key (second_sensor_id) references sensor (id)
 );
