194b1b8761dbaf6e35642675311bf9949dd00aa2
Vitali_Taran <Vitali_Taran@epam.com>
2019-05-20 10:02:02 +0300

    Created service exception

---
+++ src/main/java/com/taran/navitex/exception/NavitexServiceException.java
@@ -0,0 +1,13 @@
+package com.taran.navitex.exception;
+
+public class NavitexServiceException extends Exception {
+
+    public NavitexServiceException() {
+        super();
+    }
+
+    public NavitexServiceException(String message) {
+        super(message);
+    }
+
+}
